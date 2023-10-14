e35d84672f4fa406ce3bec1946ea1a11f235a417
Luiz Felipe <felipe.marques_80@hotmail.com>
2023-10-14 08:57:53 -0300

    Category

---
+++ src/main/java/com/felipedev/aula/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.felipedev.aula.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.felipedev.aula.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
